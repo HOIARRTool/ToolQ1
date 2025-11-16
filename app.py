83645748a1420a9fe5eecd64528a8ce98e4249f0
HOIARRTool <wilasinee.k@gmail.com>
2025-11-16 16:33:15 +0700

    Update SHS logo image styling
    Adjusted the style of the SHS logo image.

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ st.markdown(
              style="height:70px; margin-top:-1px;">
         <!-- ขยับโลโก้ SHS ลงมานิดหน่อยด้วย margin-top -->
         <img src="https://github.com/HOIARRTool/appqtbi/blob/main/logoSHS.png?raw=true"
-             style="height:40px; margin-top:9px;">
+             style="height:45px; margin-top:12px;">
     </div>
     """,
     unsafe_allow_html=True
